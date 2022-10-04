403232d5d0d43d812c862d3269e1e5ba68e6a4c8
Jack Doy <jackdoy99@icloud.com>
2022-10-04 10:39:12 +0100

    Updated state and errors

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 export default function App() {
+  const [showError, setShowError] = useState(false);
   const [movies, setMovies] = useState([]);
   const [form, setForm] = useState({
     title: "",
@@ -19,17 +20,27 @@ export default function App() {
     e.preventDefault();
     const API = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${form.title}`;
     const res = await axios.get(API);
-    const tempMovies = [...movies];
-    tempMovies.unshift(res.data);
-    console.log(tempMovies);
-    setMovies(tempMovies);
-    console.log(res.data);
+
+    if (res.data.Error !== "Movie not found!") {
+      const tempMovies = [...movies];
+      tempMovies.unshift(res.data);
+
+      // keeps only 3 movies on screen
+      if (tempMovies.length > 3) {
+        tempMovies.pop();
+      }
+      setShowError(false);
+      setMovies(tempMovies);
+    } else {
+      // handle error
+      setShowError(true);
+    }
   };
 
   return (
     <div className="App">
       <Header />
-      <Main getMovie={getMovie} handleChange={handleChange} movies={movies} />
+      <Main getMovie={getMovie} handleChange={handleChange} movies={movies} showError={showError} />
       <Footer />
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "../Main/Main.css";
 
-export default function Main({ getMovie, movies, handleChange }) {
+export default function Main({ getMovie, movies, handleChange, showError }) {
   return (
     <main className="main">
       <form className="form" onSubmit={getMovie}>
         <input name="title" placeholder={"Title of movie/show"} onChange={handleChange} />
-        <button>Explore!</button>
+        <button type="submit">Explore!</button>
       </form>
-      {movies.map((movie) => {
+      {showError && <h3 className="error">That is not a valid movie title!</h3>}
+      {movies.map((movie, index) => {
         return (
-          <div className="movie">
-            <h1 className="movie-title">{movie.Title}</h1>
+          <div className="movie" key={index}>
+            <h2 className="movie-title">{movie.Title}</h2>
             <p className="movie-plot">{movie.Plot}</p>
             <img className="movie-poster" src={movie.Poster} alt={movie.Title} />
             <p className="movie-actors">{movie.Actors}</p>
